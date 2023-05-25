d6e6c31e77fcbaf5d29681cc571bbf86156125a7
klandestino7 <klandestino6@hotmail.com>
2023-05-25 13:36:26 -0300

    atualizand o google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,10 @@
 "use client"
-import { ChakraProvider } from "@chakra-ui/react"
+import { ChakraProvider } from "@chakra-ui/react";
 
-import WithSubnavigation from "@/components/Navbar"
-import SmallWithSocial from "@/components/Footer"
+import WithSubnavigation from "@/components/Navbar";
+import SmallWithSocial from "@/components/Footer";
+
+import Script from 'next/script';
 
 export default function RootLayout({
   children,
@@ -11,11 +13,20 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
-      <head />
+
+      <head>
+        <Script strategy="lazyOnload" src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`} />
+        <Script strategy="lazyOnload">
+            {`
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+                page_path: window.location.pathname,
+                });
+            `}
+        </Script>
+      </head>
 
       <body>
         <ChakraProvider>
