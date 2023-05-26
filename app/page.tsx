ba815141c414d12e78d4c3fc25617173ede45f0d
klandestino7 <klandestino6@hotmail.com>
2023-05-25 22:44:18 -0300

    adicionando ga4

--- app/page.tsx
+++ app/page.tsx
@@ -6,9 +6,31 @@ import AboutAuthor from "@/components/AboutAuthor"
 
 import "./global.css";
 
+import Script from 'next/script';
+
 export default function Home() {
   return (
     <>
+        <Script
+          id="id-google-tag-01"
+          strategy="afterInteractive"
+          src={`https://www.googletagmanager.com/gtag/js?id=G-J2P8HKXW6Z`}
+        />
+        <Script
+          id="id-google-tag-02"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-J2P8HKXW6Z', {
+                page_path: window.location.pathname,
+              });
+            `,
+          }}
+        />
+
       <SimpleThreeColumns />
       <AboutAuthor />
       <SplitWithImage />
